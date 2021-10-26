33c64b95063ce035c0eff602ae3e7d207cb775ca
AlejandroBecLDM <nv@dekkvar*1>
2021-10-25 22:12:24 -0500

    DB connected

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
 
+//app
+const app = express()
+
+//routes
 app.get('/', (req, res) => {
     res.send('Hello from node')
+})
+
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+}).then(() => {
+    console.log('DB Connected');
 })
 
 const port = process.env.PORT || 8000
